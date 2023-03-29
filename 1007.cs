8f054dd20370402a89befbfa8eca22d7eb524b08
Igor Gassen <104468900+Kryotsz@users.noreply.github.com>
2023-03-29 12:06:04 -0300

    Add files via upload

---
+++ 1007.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D, DIFERENCA;
+            A=int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            B=int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            C=int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            D=int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            DIFERENCA=(A*B-C*D);
+            Console.WriteLine("DIFERENCA = "+DIFERENCA);
+        }
+    }
+}
